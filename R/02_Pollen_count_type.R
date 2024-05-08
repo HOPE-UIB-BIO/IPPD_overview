a7b84330c540c5e5b2125b6b1c03227b6699df6e
OndrejMottl <ondrej.mottl@gmail.com>
2024-05-08 10:28:59 +0200

    02_Pollen_count_type: plot figure 2

---
+++ R/02_Pollen_count_type.R
@@ -0,0 +1,75 @@
+#----------------------------------------------------------#
+#
+#
+#                     IPPD overview
+#
+#              Spatial distribution of count types
+#
+#
+#                       O. Mottl
+#                         2024
+#
+#----------------------------------------------------------#
+
+# Create summary of spatial distribution of various types of pollen counts
+
+#----------------------------------------------------------#
+# 1. Set up -----
+#----------------------------------------------------------#
+
+library(here)
+
+# Load configuration
+source(
+  here::here("R/00_Config_file.R")
+)
+
+
+#----------------------------------------------------------#
+# 2. Load data  -----
+#----------------------------------------------------------#
+
+ippd_data_public <-
+  readr::read_rds(
+    paste0(current_dir, "/Data/Input/ippd_data_public-2021-12-15.rds")
+  ) %>%
+  purrr::pluck("data")
+
+dplyr::glimpse(ippd_data_public)
+
+
+#----------------------------------------------------------#
+# 3. Create figure -----
+#----------------------------------------------------------#
+
+p_count <-
+  plot_data_distribution_spatial(
+    data = ippd_data_public,
+    var = "pollen_percentage",
+    coord_long = c(long_min, long_max), # [Config]
+    coord_lat = c(lat_min, lat_max), # [Config]
+    point_size = point_size, # [Config]
+    text_size = text_size, # [Config]
+    line_size = line_size, # [Config]
+    map_color_fill = map_color_fill, # [Config]
+    map_color_border = map_color_border, # [Config]"
+    caption_label = TRUE
+  )
+
+#----------------------------------------------------------#
+# 4. Save -----
+#----------------------------------------------------------#
+
+purrr::walk(
+  .x = c("pdf", "png"),
+  .f = ~ ggplot2::ggsave(
+    filename = paste0(
+      current_dir, "/Outputs/Figures/Figure_02.", .x
+    ),
+    plot = p_count,
+    width = image_width, # [Config]
+    height = image_height, # [Config]
+    units = image_units, # [Config]
+    dpi = image_dpi # [Config]
+  )
+)
