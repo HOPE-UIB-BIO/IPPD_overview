16d05c3a09a16edd35d4d9b466e725d670ce8a06
OndrejMottl <ondrej.mottl@gmail.com>
2024-05-11 14:17:45 +0200

    plot_data_barplot: allow to plot different X axis and fill value

--- R/Functions/plot_data_barplot.R
+++ R/Functions/plot_data_barplot.R
@@ -1,21 +1,30 @@
 plot_data_barplot <- function(
     data,
-    var,
+    var_x,
+    var_fill,
     custom_pallete,
     pallete_set = "Set1",
     bar_default_color = "grey30",
     bar_alpha = 1,
     text_size = 10,
     line_size = 0.1,
+    x_label_angle = 45,
+    plot_number_of_records = TRUE,
     legend_n_col = 3,
     legend_position = "bottom",
     caption_label = TRUE) {
+  if (
+    missing(var_fill)
+  ) {
+    var_fill <- var_x
+  }
+
   if (
     missing(custom_pallete)
   ) {
     var_list <-
       data %>%
-      dplyr::distinct(get(var)) %>%
+      dplyr::distinct(get(var_fill)) %>%
       purrr::pluck(1)
 
     var_list_length <- length(var_list)
@@ -36,17 +45,32 @@ plot_data_barplot <- function(
     names(custom_pallete) <- var_list
   }
 
-  p_barplot <-
+  data_summed <-
     data %>%
-    dplyr::group_by(get(var)) %>%
-    dplyr::summarise(N = dplyr::n()) %>%
-    dplyr::rename(!!var := `get(var)`) %>%
+    dplyr::group_by(get(var_x), get(var_fill)) %>%
+    dplyr::summarise(
+      .groups = "drop",
+      N = dplyr::n()
+    ) %>%
+    rlang::set_names(
+      nm = c(var_x, var_fill, "N")
+    )
+
+  if (
+    var_x == var_fill
+  ) {
+    data_summed <-
+      data_summed[-2]
+  }
+
+  p_barplot <-
+    data_summed %>%
     ggplot2::ggplot(
       ggplot2::aes(
         y = N,
-        x = get(var),
+        x = get(var_x),
         label = N,
-        fill = get(var)
+        fill = get(var_fill)
       )
     ) +
     ggplot2::geom_bar(
@@ -55,24 +79,30 @@ plot_data_barplot <- function(
       linewidth = line_size,
       alpha = bar_alpha
     ) +
-    ggplot2::geom_text(
-      size = text_size / (ggplot2::.pt),
-      vjust = 0,
-      nudge_y = 0
-    ) +
     ggplot2::scale_color_manual(values = custom_pallete) +
     ggplot2::scale_fill_manual(values = custom_pallete) +
     ggplot2::theme(
-      text = ggplot2::element_text(size = text_size),
-      plot.title = ggplot2::element_text(size = text_size),
-      axis.text.x = ggplot2::element_text(size = text_size, angle = 45, hjust = 1, vjust = 1),
-      line = ggplot2::element_line(size = line_size),
+      text = ggplot2::element_text(
+        size = text_size
+      ),
+      plot.title = ggplot2::element_text(
+        size = text_size
+      ),
+      axis.text.x = ggplot2::element_text(
+        size = text_size,
+        angle = x_label_angle,
+        hjust = 1,
+        vjust = 1
+      ),
+      line = ggplot2::element_line(
+        size = line_size
+      ),
       legend.title = ggplot2::element_blank(),
       legend.position = legend_position
     ) +
     ggplot2::labs(
       y = "Number of records",
-      x = var
+      x = var_x
     ) +
     ggplot2::guides(
       fill = ggplot2::guide_legend(
@@ -82,6 +112,24 @@ plot_data_barplot <- function(
         ncol = legend_n_col
       )
     )
+
+  if (
+    isTRUE(plot_number_of_records)
+  ) {
+    p_barplot <-
+      p_barplot +
+      ggplot2::geom_text(
+        data = data_summed,
+        ggplot2::aes(
+          y = N,
+          x = get(var_x),
+          label = N
+        ),
+        size = text_size / (ggplot2::.pt),
+        vjust = 0,
+        nudge_y = 0
+      )
+  }
 
   if (
     isTRUE(caption_label)
